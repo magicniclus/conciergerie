4336bb949b5eda33fdd8069b8c2b53f64bb37a81
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-06-26 14:36:38 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,23 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        dark: "#1E1F22",
+        white: "#FFFFFF",
+        beige: "#F2E6D0",
+        orange: "#F27E63",
+        purple: "#585FCC",
+        blue: "#CAE6FE",
+        blueLight: "#DCE0F2",
+        green: "#8FD8B4",
+        gray: "#8A909A",
+        yellow: "#FABF47",
+      },
+    },
   },
   plugins: [],
 };
