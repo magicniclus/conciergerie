eb3866f9b708f62b43119a6536c66f7698cf07bf
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-06-26 14:36:30 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Classic from "../components/layouts/Classic";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold">Hello world!</h1>;
+  return <Classic></Classic>;
 }
