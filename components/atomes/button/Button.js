beb563807e4cc05302e4aaaf3128239072c86630
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-06-27 15:23:36 +0200

    Create Button.js

---
+++ components/atomes/button/Button.js
@@ -0,0 +1,18 @@
+import { ArrowSmallRightIcon } from "@heroicons/react/24/outline";
+import React from "react";
+
+const Button = (props) => {
+  const backgroundColor = props.backgroundColor || "bg-white";
+  const textColor = props.textColor || "text-orange";
+  const link = props.link || "/";
+  return (
+    <a
+      url={link}
+      className={`${backgroundColor} ${textColor} flex items-center px-5 py-3 w-max font-semibold rounded-lg  hover:bg-whiteHover hover:text-orange`}
+    >
+      Prendre rendez-vous <ArrowSmallRightIcon className="w-5 h-5 ml-2" />
+    </a>
+  );
+};
+
+export default Button;
