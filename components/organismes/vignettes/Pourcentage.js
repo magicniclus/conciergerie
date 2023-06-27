d2f40cefb2c4359ea597ed51209c3fab5b28c417
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-06-27 17:08:24 +0200

    Update Pourcentage.js

--- components/organismes/vignettes/Pourcentage.js
+++ components/organismes/vignettes/Pourcentage.js
@@ -1,7 +1,24 @@
 import React from "react";
+import Button from "../../atomes/button/Button";
 
 const Pourcentage = () => {
-  return <div></div>;
+  return (
+    <div className="text-dark max-w-[200px]">
+      <p className="font-xs mb35">
+        Le tarif sur commission <br />à partir de
+      </p>
+      <div className="flex flex-row items-center mb-3">
+        <p className="text-orange lg:text-5xl text-3xl">20%</p>
+        <p className="text-base font-semibold ml-5">
+          de vos <br /> revenus <br /> locatifs
+        </p>
+      </div>
+      <p className="font-xs mb-3">
+        La commission dépend du <br /> profil de votre bien.
+      </p>
+      <Button backgroundColor="bg-orange" textColor="text-white" />
+    </div>
+  );
 };
 
 export default Pourcentage;
