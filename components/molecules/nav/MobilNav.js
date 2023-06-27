cab985981952baebbd67c1edc81908180debff85
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-06-27 14:19:48 +0200

    Update MobilNav.js

--- components/molecules/nav/MobilNav.js
+++ components/molecules/nav/MobilNav.js
@@ -7,7 +7,7 @@ import SelectedLanguage from "../selected/SelectedLanguage";
 const MobilNav = (props) => {
   const { openMenu, setOpenMenu } = props;
   return (
-    <div className="absolute left-0 top-0 w-screen min-h-screen px-6 lg:px-8 py-5 bg-white z-50">
+    <div className="absolute left-0 top-0 w-screen min-h-screen  px-3 xs:px-6 lg:px-8 py-5 bg-white z-50">
       <div>
         <div className="w-full flex justify-between">
           <img src="/img/logos/littleLogo.png" className="w-7 object-contain" />
