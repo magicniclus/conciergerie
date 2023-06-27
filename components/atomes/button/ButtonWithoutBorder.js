9378a5753b9ff276d384e68abdb6cf6fd37281da
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-06-27 16:29:51 +0200

    Update ButtonWithoutBorder.js

--- components/atomes/button/ButtonWithoutBorder.js
+++ components/atomes/button/ButtonWithoutBorder.js
@@ -4,7 +4,10 @@ import { ArrowSmallRightIcon } from "@heroicons/react/24/outline";
 const ButtonWithoutBorder = (props) => {
   const link = props.link || "/";
   return (
-    <a className="flex text-orange items-center font-semibold" url={link}>
+    <a
+      className="flex text-orange items-center font-semibold  cursor-pointer"
+      url={link}
+    >
       Prendre rendez-vous
       <ArrowSmallRightIcon className="w-5 h-5 ml-2" />
     </a>
