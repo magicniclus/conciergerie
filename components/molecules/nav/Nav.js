83c9d3dcdf9d51500eb43ac702e60260a23f9efe
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-06-27 10:20:51 +0200

    Update Nav.js

--- components/molecules/nav/Nav.js
+++ components/molecules/nav/Nav.js
@@ -4,10 +4,10 @@ const Nav = () => {
   return (
     <nav>
       <div>
-        <ul className="flex md:flex-row flex-col sm:font-semibold text-dark text-xl sm:text-sm">
-          <li className="flex md:mb-0 mb-5">
+        <ul className="flex lg:flex-row flex-col lg:font-semibold text-dark text-xl lg:text-sm">
+          <li className="flex lg:mb-0 mb-5">
             <a href="/">Nos biens en gestion</a>
-            <div className="md:flex hidden h-full w-0.5 bg-dark mx-4"></div>
+            <div className="lg:flex hidden h-full w-0.5 bg-dark mx-4"></div>
           </li>
           <li>
             <a href="/a-propos">Nos services</a>
