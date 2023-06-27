333fbb8e43ea39cb448beee3e52e8101c0fcbc6d
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-06-27 15:23:47 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Classic from "../components/layouts/Classic";
+import WhoWeAre from "../components/organismes/container/WhoWeAre";
 import AccueilHero from "../components/organismes/hero/AccueilHero";
 
 export default function Home() {
   return (
     <Classic>
       <AccueilHero />
+      <WhoWeAre />
     </Classic>
   );
 }
