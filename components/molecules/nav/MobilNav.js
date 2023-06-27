142b3fd8ad77842b3fd602eb3c040f6e94e1d7d1
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-06-27 10:12:29 +0200

    Create MobilNav.js

---
+++ components/molecules/nav/MobilNav.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import Nav from "./Nav";
+import ButtonWithBackground from "../../atomes/button/ButtonWithBackground";
+import SelectedLanguage from "../selected/SelectedLanguage";
+
+const MobilNav = (props) => {
+  const { openMenu, setOpenMenu } = props;
+  return (
+    <div className="absolute left-0 top-0 w-screen min-h-screen px-6 lg:px-8 py-5 bg-gray-200">
+      <div>
+        <div className="w-full flex justify-between">
+          <img src="/img/logos/littleLogo.png" className="w-7 object-contain" />
+          <XMarkIcon
+            onClick={() => setOpenMenu((openMenu) => !openMenu)}
+            className="w-10 text-dark"
+            aria-hidden="true"
+          />
+        </div>
+        <div className="flex flex-col items-start mt-14 min-h-[180px] justify-between">
+          <Nav />
+          <ButtonWithBackground />
+          <SelectedLanguage />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MobilNav;
