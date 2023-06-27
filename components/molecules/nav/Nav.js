f4b7b95fdc92df0520ef298ffd6e955fff3d9310
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-06-27 10:12:31 +0200

    Update Nav.js

--- components/molecules/nav/Nav.js
+++ components/molecules/nav/Nav.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Nav = () => {
-  return <nav></nav>;
+  return (
+    <nav>
+      <div>
+        <ul className="flex md:flex-row flex-col sm:font-semibold text-dark text-xl sm:text-sm">
+          <li className="flex md:mb-0 mb-5">
+            <a href="/">Nos biens en gestion</a>
+            <div className="md:flex hidden h-full w-0.5 bg-dark mx-4"></div>
+          </li>
+          <li>
+            <a href="/a-propos">Nos services</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
