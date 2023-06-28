fe7e23fa4038a9acfa1300c3e89522a761de2973
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-06-28 14:37:51 +0200

    Update OnlyPlatform.js

--- components/organismes/container/OnlyPlatform.js
+++ components/organismes/container/OnlyPlatform.js
@@ -4,13 +4,13 @@ import { CheckIcon } from "@heroicons/react/24/outline";
 const OnlyPlatform = () => {
   const logos = [
     {
-      logo: "/img/logos/aibnb.png",
-    },
-    {
-      logo: "/img/logos/booking.png",
+      logo: "/img/logos/airbnb.png",
     },
     {
       logo: "/img/logos/homelike.png",
+    },
+    {
+      logo: "/img/logos/booking.png",
     },
     {
       logo: "/img/logos/abritel.png",
@@ -32,41 +32,10 @@ const OnlyPlatform = () => {
             key={index}
             src={logo.logo}
             alt="logo"
-            className="h-32 object-contain my-8"
+            className="h-24 object-contain my-8"
           />
         ))}
       </div>
-      {/* <div className="lg:hidden flex flex-wrap mt-7 justify-between">
-        <div className="flex">
-          <h3 className="ml-3 text-2xl text-dark lg:text-4xl font-semibold">
-            Airbnb
-          </h3>
-        </div>
-        <div className="flex ml-3">
-          <h3 className="text-2xl text-dark lg:text-4xl font-semibold">|</h3>
-        </div>
-        <div className="flex">
-          <h3 className="ml-3 text-2xl text-dark lg:text-4xl font-semibold">
-            Booking
-          </h3>
-        </div>
-        <div className="flex ml-3">
-          <h3 className="text-2xl text-dark lg:text-4xl font-semibold">|</h3>
-        </div>
-        <div className="flex">
-          <h3 className="ml-3 text-2xl text-dark lg:text-4xl font-semibold">
-            Abritel
-          </h3>
-        </div>
-        <div className="flex ml-3">
-          <h3 className="text-2xl text-dark lg:text-4xl font-semibold">|</h3>
-        </div>
-        <div className="flex">
-          <h3 className="ml-3 text-2xl text-dark lg:text-4xl font-semibold">
-            Homelike
-          </h3>
-        </div>
-      </div> */}
     </div>
   );
 };
