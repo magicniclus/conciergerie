41901baf39b7bdf0340e74141bb352a623bf28e0
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-06-28 13:33:33 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Classic from "../components/layouts/Classic";
 import AllServices from "../components/organismes/container/AllServices";
+import CardsPrestation from "../components/organismes/container/CardsPrestation";
 import WhoWeAre from "../components/organismes/container/WhoWeAre";
 import AccueilHero from "../components/organismes/hero/AccueilHero";
 
@@ -9,6 +10,7 @@ export default function Home() {
       <AccueilHero />
       <WhoWeAre />
       <AllServices />
+      <CardsPrestation />
     </Classic>
   );
 }
