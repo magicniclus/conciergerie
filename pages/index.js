8f22f1462045d93c4ed84a9b54eb24a67b7a4b70
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-06-28 14:07:53 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Classic from "../components/layouts/Classic";
 import AllServices from "../components/organismes/container/AllServices";
 import CardsPrestation from "../components/organismes/container/CardsPrestation";
+import OnlyPlatform from "../components/organismes/container/OnlyPlatform";
+import WhatWorkWithWe from "../components/organismes/container/WhatWorkWithWe";
 import WhoWeAre from "../components/organismes/container/WhoWeAre";
 import AccueilHero from "../components/organismes/hero/AccueilHero";
 
@@ -11,6 +13,8 @@ export default function Home() {
       <WhoWeAre />
       <AllServices />
       <CardsPrestation />
+      <WhatWorkWithWe />
+      <OnlyPlatform />
     </Classic>
   );
 }
