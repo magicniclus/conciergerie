485704f21351d1e4b2033c5f0c6bdfd9604ea534
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-06-29 15:33:58 +0200

    Update OnlyPlatform.js

--- components/organismes/container/OnlyPlatform.js
+++ components/organismes/container/OnlyPlatform.js
@@ -1,4 +1,8 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const OnlyPlatform = () => {
   const logos = [
@@ -15,17 +19,53 @@ const OnlyPlatform = () => {
       logo: "/img/logos/abritel.png",
     },
   ];
+
+  const h2Ref = useRef(null);
+  const h3Ref = useRef(null);
+  const imgRef = useRef(null);
+
+  useEffect(() => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: h2Ref.current,
+        start: "top 80%",
+        toggleActions: "play none none none",
+      },
+    });
+
+    tl.fromTo(
+      [h2Ref.current, h3Ref.current, imgRef.current],
+      {
+        opacity: 0,
+        y: 50,
+      },
+      {
+        duration: 0.5,
+        opacity: 1,
+        y: 0,
+        stagger: 0.3,
+        ease: "power1.out",
+      }
+    );
+  }, []);
+
   return (
     <div className="w-full max-w-[1450px] relative mx-auto px-3 xs:px-6 lg:px-8 py-14 bg-white">
-      <h2 className="text-2xl text-dark lg:text-4xl font-semibold mb-7 ">
+      <h2
+        ref={h2Ref}
+        className="text-2xl text-dark lg:text-4xl font-semibold mb-7 "
+      >
         Gestion optimale avec les meilleures plateformes
       </h2>
-      <h3 className="text-dark font-xs lg:w-8/12 w-full">
+      <h3 ref={h3Ref} className="text-dark font-xs lg:w-8/12 w-full">
         Nous mettons en avant votre bien sur diverses plateformes de location
         pour des séjours de courte et moyenne durée, et ce, de manière
         simultanée.
       </h3>
-      <div className="flex justify-between items-center flex-wrap mt-7">
+      <div
+        ref={imgRef}
+        className="flex justify-between items-center flex-wrap mt-7"
+      >
         {logos.map((logo, index) => (
           <img
             key={index}
