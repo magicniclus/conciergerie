b449931f831c4ca29b285a8347e666569056f295
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-06-29 15:33:56 +0200

    Update Faq.js

--- components/organismes/container/Faq.js
+++ components/organismes/container/Faq.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Question from "../../molecules/questions/Question";
 import Button from "../../atomes/button/Button";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Faq = () => {
   const questionContent = [
@@ -37,13 +41,47 @@ const Faq = () => {
         "Nous faisons tout notre possible pour prévenir ce genre de situations en sélectionnant soigneusement les locataires. Cependant, en cas de dommages, nous vous aiderons à gérer la situation, y compris l'utilisation de dépôts de garantie et de politiques d'assurance si nécessaire.",
     },
   ];
+
+  const h2Ref = useRef(null);
+  const h3Ref = useRef(null);
+  const questionRef = useRef(null);
+  const imgRef = useRef(null);
+
+  useEffect(() => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: h2Ref.current,
+        start: "top 80%",
+        toggleActions: "play none none none",
+      },
+    });
+
+    tl.fromTo(
+      [h2Ref.current, h3Ref.current, questionRef.current, imgRef.current],
+      {
+        opacity: 0,
+        y: 50,
+      },
+      {
+        duration: 0.5,
+        opacity: 1,
+        y: 0,
+        stagger: 0.3,
+        ease: "power1.out",
+      }
+    );
+  }, []);
+
   return (
     <div className="bg-white w-full max-w-[1450px] relative mx-auto px-3 xs:px-6 lg:px-8 pb-14">
-      <h2 className="text-2xl text-dark lg:text-4xl font-semibold mb-7 ">
+      <h2
+        ref={h2Ref}
+        className="text-2xl text-dark lg:text-4xl font-semibold mb-7 "
+      >
         F.A.Q
       </h2>
       <div className="flex lg:flex-row flex-col">
-        <div className="w-full lg:w-1/2 lg:mb-0 mb-10">
+        <div ref={questionRef} className="w-full lg:w-1/2 lg:mb-0 mb-10">
           {questionContent.map((el, index) => (
             <Question
               key={index}
@@ -52,7 +90,10 @@ const Faq = () => {
             />
           ))}
         </div>
-        <div className="bg-gray-50 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+        <div
+          ref={imgRef}
+          className="bg-gray-50 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2"
+        >
           <img
             className="aspect-[3/2] lg:hidden block object-cover lg:aspect-auto h-full w-full relative"
             src="/img/logos/piscine.png"
@@ -63,7 +104,7 @@ const Faq = () => {
             className="absolute top-1/2 left-1/2 bg-white px-6 py-10 transform -translate-x-1/2 -translate-y-1/2 lg:block hidden"
             style={{ borderRadius: "0 150px 0 0" }}
           >
-            <h3 className="lg:text-5xl text-3xl font-dark mb-8">
+            <h3 ref={h3Ref} className="lg:text-5xl text-3xl font-dark mb-8">
               <span className="text-orange ">Bienvenue</span> dans notre section
               FAQ!
               <br />
