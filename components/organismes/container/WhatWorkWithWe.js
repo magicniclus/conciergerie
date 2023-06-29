30bd5cccd3b9bf0915bd51b91da9add3941d1f20
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-06-29 15:34:00 +0200

    Update WhatWorkWithWe.js

--- components/organismes/container/WhatWorkWithWe.js
+++ components/organismes/container/WhatWorkWithWe.js
@@ -1,13 +1,49 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Button from "../../atomes/button/Button";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const WhatWorkWithWe = () => {
+  const titleRef = useRef(null);
+  const pRef = useRef(null);
+  const buttonRef = useRef(null);
+
+  useEffect(() => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: titleRef.current,
+        start: "top 80%",
+        toggleActions: "play none none none",
+      },
+    });
+
+    tl.fromTo(
+      [titleRef.current, pRef.current, buttonRef.current],
+      {
+        opacity: 0,
+        y: 50,
+      },
+      {
+        duration: 0.5,
+        opacity: 1,
+        y: 0,
+        stagger: 0.3,
+        ease: "power1.out",
+      }
+    );
+  }, []);
+
   return (
     <div className="bg-beige w-full max-w-[1450px] relative mx-auto px-3 xs:px-6 lg:px-8 py-14">
-      <h2 className="text-2xl text-dark lg:text-4xl font-semibold mb-7 ">
+      <h2
+        ref={titleRef}
+        className="text-2xl text-dark lg:text-4xl font-semibold mb-7 "
+      >
         Pourquoi travailler avec nous ?
       </h2>
-      <p className="text-dark font-xs mb-7">
+      <p ref={pRef} className="text-dark font-xs mb-7">
         Choisir de travailler avec Conciergerie Castera, c'est choisir une
         expérience sans égal en matière de gestion de propriété. En tant
         qu'entreprise familiale issue du monde de la location Airbnb, nous
@@ -20,7 +56,9 @@ const WhatWorkWithWe = () => {
         souci. Nous sommes votre partenaire de confiance, dédié à transformer
         votre bien en source de revenus lucrative.
       </p>
-      <Button />
+      <div ref={buttonRef}>
+        <Button />
+      </div>
     </div>
   );
 };
