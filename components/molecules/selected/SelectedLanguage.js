758563a3e15eb11cdb9d321fb75777847cac41bb
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-06-30 13:08:35 +0200

    Update SelectedLanguage.js

--- components/molecules/selected/SelectedLanguage.js
+++ components/molecules/selected/SelectedLanguage.js
@@ -4,6 +4,11 @@ import { ChevronDownIcon } from "@heroicons/react/24/outline";
 const SelectedLanguage = () => {
   const language = ["fr", "en", "es", "it", "de"];
   const [selectedLanguage, setSelectedLanguage] = useState("fr");
+
+  const handleChange = (event) => {
+    setSelectedLanguage(event.target.value);
+  };
+
   return (
     <div className="relative">
       <ChevronDownIcon
@@ -12,17 +17,12 @@ const SelectedLanguage = () => {
       />
       <select
         required
+        value={selectedLanguage}
+        onChange={handleChange}
         className=" cursor-pointer block bg-gray-50 rounded-md border-dark text-gray-800 shadow-sm ring-1 ring-inset ring-dark placeholder:text-dark focus:ring-2 focus:ring-inset focus:ring-white sm:text-sm sm:leading-6 relative px-2.5 py-0.5 text-xl"
       >
-        <option value="" disabled selected>
-          {selectedLanguage}
-        </option>
         {language.map((prestation, index) => (
-          <option
-            onClick={() => setSelectedLanguage(prestation)}
-            key={index}
-            value={prestation}
-          >
+          <option key={index} value={prestation}>
             {prestation}
           </option>
         ))}
