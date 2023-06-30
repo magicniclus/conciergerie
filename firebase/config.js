9af9da17b27e9f20641515d5d782b6bfeb8d49d4
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-06-30 17:04:36 +0200

    Update config.js

--- firebase/config.js
+++ firebase/config.js
@@ -2,16 +2,16 @@ import { initializeApp, getApps } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "API",
-//   authDomain: "adsventure-66776.firebaseapp.com",
-//   projectId: "adsventure-66776",
-//   storageBucket: "adsventure-66776.appspot.com",
-//   messagingSenderId: "565103293150",
-//   appId: "1:565103293150:web:9d20355e1ba8a47170c84f",
-//   databaseURL:
-//     "https://adsventure-66776-default-rtdb.europe-west1.firebasedatabase.app",
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyCXZRlN4EMVqlz51LSiNODkHlrcJVkjNwc",
+  authDomain: "conciergerie-3adc8.firebaseapp.com",
+  databaseURL:
+    "https://conciergerie-3adc8-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "conciergerie-3adc8",
+  storageBucket: "conciergerie-3adc8.appspot.com",
+  messagingSenderId: "335932845266",
+  appId: "1:335932845266:web:352fccb316b182df5bd10b",
+};
 
 // Initialize Firebase
 let app;
