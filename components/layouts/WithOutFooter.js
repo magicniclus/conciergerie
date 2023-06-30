9b2e9c1eaa9471ff22ef85dfdd724314b7b95b94
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-06-30 13:08:33 +0200

    Create WithOutFooter.js

---
+++ components/layouts/WithOutFooter.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Head from "next/head";
+import Header from "../organismes/header/Header";
+
+const WithOutFooter = (props) => {
+  const description = props.description || null;
+  const title = props.title || null;
+  const canonical = props.canonical || "";
+  const robots = props.robots || "follow";
+  const tags = props.tags || null;
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="robots" content={"index, " + robots} />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="language" content="fr" />
+        <link
+          rel="canonical"
+          href={"https://www.castera-conciergerie.fr/" + canonical}
+        />
+        <meta name="keywords" content={tags} />
+        <link rel="icon" href="/img/logos/logo3.png" />
+      </Head>
+      <Header />
+      <main>{props.children}</main>
+    </>
+  );
+};
+
+export default WithOutFooter;
