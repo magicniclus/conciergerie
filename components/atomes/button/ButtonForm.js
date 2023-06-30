03397a63e61dd928675bc9ce56b1af6a0cd73567
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-06-30 13:08:20 +0200

    Create ButtonForm.js

---
+++ components/atomes/button/ButtonForm.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ButtonForm = (props) => {
+  const backgroundColor = props.backgroundColor || "bg-white";
+  const textColor = props.textColor || "text-orange";
+  const link = props.link || "/";
+  return (
+    <a
+      url={link}
+      className={`${backgroundColor} ${textColor} flex items-center px-4 py-2 w-max max-w-full font-semibold rounded-lg hover:bg-whiteHover hover:text-orange cursor-pointer drop-shadow-lg`}
+    >
+      Envoyer
+    </a>
+  );
+};
+
+export default ButtonForm;
