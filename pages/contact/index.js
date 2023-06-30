bfc407645d54da753e7781d3c71c2b4d61face92
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-06-30 13:08:44 +0200

    Create index.js

---
+++ pages/contact/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import WithOutFooter from "../../components/layouts/WithOutFooter";
+import ContactRdv from "../../components/organismes/container/ContactRdv";
+
+const index = () => {
+  return (
+    <WithOutFooter title="Contact">
+      <ContactRdv />
+    </WithOutFooter>
+  );
+};
+
+export default index;
