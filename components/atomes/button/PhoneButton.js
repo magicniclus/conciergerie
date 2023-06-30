1ae552a2c5302c68fd3d53ddf812e5e9997e8518
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-06-30 13:08:24 +0200

    Create PhoneButton.js

---
+++ components/atomes/button/PhoneButton.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { PhoneIcon } from "@heroicons/react/24/solid";
+
+const PhoneButton = () => {
+  return (
+    <a
+      className="bg-orange rounded-lg p-3 text-white flex cursor-pointer hover:drop-shadow-lg transition-shadow"
+      href="tel:0631420045"
+    >
+      <PhoneIcon className="w-5" />
+      <span className="ml-2 font-semibold">06 31 42 00 45</span>
+    </a>
+  );
+};
+
+export default PhoneButton;
