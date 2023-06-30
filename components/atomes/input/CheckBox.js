a3fbc30d775b3cf6cfc5604875225b8256a8bb76
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-06-30 13:08:28 +0200

    Create CheckBox.js

---
+++ components/atomes/input/CheckBox.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const CheckBox = (props) => {
+  const value = props.value || "";
+  const setValue = props.setValue || null;
+
+  return (
+    <input
+      type="checkbox"
+      id="rgpd"
+      name="rgpd"
+      required
+      className="border-dark bg-slate-50 border rounded-lg"
+      onChange={(e) => setValue(value ? true : false)}
+    />
+  );
+};
+
+export default CheckBox;
