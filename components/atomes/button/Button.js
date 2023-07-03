f78140f90798738f0e894e5ebbae115b241889c9
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-07-03 09:59:22 +0200

    Update Button.js

--- components/atomes/button/Button.js
+++ components/atomes/button/Button.js
@@ -4,10 +4,10 @@ import React from "react";
 const Button = (props) => {
   const backgroundColor = props.backgroundColor || "bg-white";
   const textColor = props.textColor || "text-orange";
-  const link = props.link || "/";
+  const link = props.link || "/contact";
   return (
     <a
-      url={link}
+      href={link}
       className={`${backgroundColor} ${textColor} flex items-center px-4 py-2 w-max max-w-full font-semibold rounded-lg hover:bg-whiteHover hover:text-orange cursor-pointer`}
     >
       Prendre rendez-vous <ArrowSmallRightIcon className="w-5 h-5 ml-2" />
