1a2e68895578d926b389816f1045596ccedd7fc6
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-07-03 11:36:44 +0200

    Create index.js

---
+++ pages/mentions-legales/index.js
@@ -0,0 +1,227 @@
+import React from "react";
+import Classic from "../../components/layouts/Classic";
+
+const index = () => {
+  return (
+    <Classic>
+      <div className="w-full max-w-[1450px] relative mx-auto px-3 xs:px-6 lg:px-32 pb-5">
+        <h1 className="text-dark xs:w-full w-10/12 text-2xl lg:text-4xl font-semibold my-7">
+          Mentions légales
+        </h1>
+        <h2 class="text-dark xs:w-full w-10/12 text-xl lg:text-2xl font-semibold mb-7">
+          1-Introduction
+        </h2>
+
+        <h3 class="text-dark xs:w-full w-10/12 text-lg lg:text-xl font-semibold mb-7">
+          Objet des mentions légales
+        </h3>
+        <p class="text-dark xs:w-full w-10/12 font-xs mb-7">
+          L'objet des présentes mentions légales est de définir les conditions
+          générales d'utilisation du site web Conciergerie-Castera. Ces
+          conditions sont applicables à toute personne visitant ou utilisant le
+          site, quel que soit le dispositif utilisé.
+        </p>
+
+        <h3 class="text-dark xs:w-full w-10/12 text-lg lg:text-xl font-semibold mb-7">
+          Acceptation des mentions légales
+        </h3>
+        <p class="text-dark xs:w-full w-10/12 font-xs mb-7">
+          En accédant à ce site et en l'utilisant, vous vous engagez à respecter
+          ces mentions légales et à les accepter sans réserve. Si vous
+          n'acceptez pas ces conditions, vous ne devez pas utiliser ce site.
+        </p>
+
+        <h2 class="text-dark xs:w-full w-10/12 text-xl lg:text-2xl font-semibold mb-7">
+          Informations générales
+        </h2>
+
+        <h3 class="text-dark xs:w-full w-10/12 text-lg lg:text-xl font-semibold mb-7">
+          Conciergerie-Castera
+        </h3>
+        <p class="text-dark xs:w-full w-10/12 font-xs mb-7">
+          Le site Conciergerie-Castera est géré par la société Nicolas Castera,
+          une entreprise dûment enregistrée conformément aux lois françaises.
+        </p>
+
+        <h3 class="text-dark xs:w-full w-10/12 text-lg lg:text-xl font-semibold mb-7">
+          Adresse du siège social
+        </h3>
+        <p class="text-dark xs:w-full w-10/12 font-xs mb-7">
+          Le siège social de Nicolas Castera est situé à 22 rue de libourne
+          33100 Bordeaux.
+        </p>
+
+        <h2 class="text-dark xs:w-full w-10/12 text-xl lg:text-2xl font-semibold mb-7">
+          Responsabilité
+        </h2>
+
+        <h3 class="text-dark xs:w-full w-10/12 text-lg lg:text-xl font-semibold mb-7">
+          Responsabilité de l'agence Castera
+        </h3>
+        <p class="text-dark xs:w-full w-10/12 font-xs mb-7">
+          L'agence Castera s'efforce d'assurer au mieux de ses possibilités,
+          l'exactitude et la mise à jour des informations diffusées sur le site,
+          dont elle se réserve le droit de corriger, à tout moment et sans
+          préavis, le contenu.
+        </p>
+
+        <h3 class="text-dark xs:w-full w-10/12 text-lg lg:text-xl font-semibold mb-7">
+          Limitation de responsabilité pour les informations fournies sur le
+          site
+        </h3>
+        <p class="text-dark xs:w-full w-10/12 font-xs mb-7">
+          Cependant, l'agence Castera ne peut garantir l'exactitude, la
+          précision ou l'exhaustivité des informations mises à disposition sur
+          ce site et la responsabilité de l'éditeur ne pourra être engagée en
+          cas d'erreur ou d'omission.
+        </p>
+        <h2 class="text-dark xs:w-full w-10/12 text-xl lg:text-2xl font-semibold mb-7">
+          3-Propriété intellectuelle
+        </h2>
+
+        <h3 class="text-dark xs:w-full w-10/12 text-lg lg:text-xl font-semibold mb-7">
+          Droits d'auteur et droits de propriété intellectuelle
+        </h3>
+        <p class="text-dark xs:w-full w-10/12 font-xs mb-7">
+          Tous les contenus présents, tels que les textes, graphiques, logos,
+          images, photographies, vidéos, sons, etc., sur le site de
+          Conciergerie-Castera sont protégés par les lois sur le droit d'auteur
+          et la propriété intellectuelle. Aucune exploitation commerciale,
+          reproduction, représentation, utilisation, adaptation, modification,
+          incorporation, traduction, commercialisation, partielle ou intégrale
+          de ces éléments ne pourra en être faite sans l'accord préalable et
+          écrit de Conciergerie-Castera, à l'exception de l'utilisation pour
+          usage privé sous réserve des dispositions différentes voire plus
+          restrictives du code de la propriété intellectuelle.
+        </p>
+
+        <h3 class="text-dark xs:w-full w-10/12 text-lg lg:text-xl font-semibold mb-7">
+          Utilisation des contenus du site
+        </h3>
+        <p class="text-dark xs:w-full w-10/12 font-xs mb-7">
+          Les utilisateurs sont autorisés à consulter et à télécharger les
+          matériaux du site pour un usage personnel et non commercial
+          uniquement, et à condition de respecter toutes les notifications de
+          droits d'auteur et autres notifications de propriété contenues dans
+          les matériaux. Vous ne pouvez pas modifier les matériaux de ce site ni
+          les reproduire, les exposer en public, les distribuer ou les utiliser
+          à des fins publiques ou commerciales sans le consentement écrit
+          préalable de Conciergerie-Castera.
+        </p>
+        <h2 class="text-dark xs:w-full w-10/12 text-xl lg:text-2xl font-semibold mb-7">
+          4-Collecte et utilisation des données personnelles
+        </h2>
+
+        <h3 class="text-dark xs:w-full w-10/12 text-lg lg:text-xl font-semibold mb-7">
+          Politique de confidentialité
+        </h3>
+        <p class="text-dark xs:w-full w-10/12 font-xs mb-7">
+          La protection de vos données personnelles est une priorité pour
+          Conciergerie-Castera. Nous nous engageons à respecter votre vie privée
+          et à protéger les informations que vous nous fournissez. Pour plus
+          d'informations sur la manière dont nous collectons, utilisons et
+          protégeons vos données personnelles, veuillez consulter notre
+          Politique de confidentialité.
+        </p>
+
+        <h3 class="text-dark xs:w-full w-10/12 text-lg lg:text-xl font-semibold mb-7">
+          Consentement de l'utilisateur
+        </h3>
+        <p class="text-dark xs:w-full w-10/12 font-xs mb-7">
+          En utilisant notre site, vous consentez à la collecte et à
+          l'utilisation de vos données personnelles comme décrit dans notre
+          Politique de confidentialité. Si nous décidons de modifier notre
+          Politique de confidentialité, nous publierons ces modifications sur
+          cette page pour que vous sachiez toujours quelles informations nous
+          collectons, comment nous les utilisons et dans quelles circonstances,
+          le cas échéant, nous les divulguons.
+        </p>
+
+        <h3 class="text-dark xs:w-full w-10/12 text-lg lg:text-xl font-semibold mb-7">
+          Cookies et technologies similaires
+        </h3>
+        <p class="text-dark xs:w-full w-10/12 font-xs mb-7">
+          Nous utilisons des cookies et des technologies similaires pour
+          améliorer votre expérience sur notre site, analyser comment vous
+          l'utilisez et à des fins publicitaires. Vous pouvez refuser certains
+          types de cookies ou obtenir plus d'informations sur la manière dont
+          nous utilisons ces technologies dans notre Politique relative aux
+          cookies.
+        </p>
+        <h2 class="text-dark xs:w-full w-10/12 text-xl lg:text-2xl font-semibold mb-7">
+          5-Liens vers des sites tiers
+        </h2>
+
+        <h3 class="text-dark xs:w-full w-10/12 text-lg lg:text-xl font-semibold mb-7">
+          Responsabilité concernant les liens externes
+        </h3>
+        <p class="text-dark xs:w-full w-10/12 font-xs mb-7">
+          Le site de Conciergerie-Castera peut contenir des liens vers des sites
+          internet tiers. Ces liens sont fournis uniquement pour votre
+          commodité. L'inclusion de ces liens ne signifie pas que
+          Conciergerie-Castera approuve, garantit ou recommande ces sites.
+          Conciergerie-Castera n'a aucun contrôle sur le contenu de ces sites
+          tiers et n'assume aucune responsabilité pour eux ou pour toute perte
+          ou dommage qui pourrait résulter de votre utilisation de ceux-ci.
+          Lorsque vous quittez notre site, nos politiques ne s'appliquent plus.
+          Par conséquent, nous vous conseillons de lire les politiques de
+          confidentialité et les conditions d'utilisation de chaque site que
+          vous visitez.
+        </p>
+        <h2 class="text-dark xs:w-full w-10/12 text-xl lg:text-2xl font-semibold mb-7">
+          6-Modifications des mentions légales
+        </h2>
+
+        <h3 class="text-dark xs:w-full w-10/12 text-lg lg:text-xl font-semibold mb-7">
+          Droit de modifier les mentions légales
+        </h3>
+        <p class="text-dark xs:w-full w-10/12 font-xs mb-7">
+          Conciergerie-Castera se réserve le droit de modifier ou de mettre à
+          jour ces mentions légales à tout moment et sans préavis. Ces
+          modifications entreront en vigueur immédiatement après leur
+          publication sur le site. En continuant à utiliser le site après toute
+          modification, vous acceptez d'être lié par les nouvelles mentions
+          légales.
+        </p>
+
+        <h3 class="text-dark xs:w-full w-10/12 text-lg lg:text-xl font-semibold mb-7">
+          Notification des modifications
+        </h3>
+        <p class="text-dark xs:w-full w-10/12 font-xs mb-7">
+          Toutes les modifications apportées à ces mentions légales seront
+          publiées sur cette page. Nous vous recommandons de consulter
+          régulièrement cette page pour rester informé de toute modification.
+          Votre utilisation continue du site après la publication des
+          modifications constitue votre acceptation de ces modifications.
+        </p>
+        <h2 class="text-dark xs:w-full w-10/12 text-xl lg:text-2xl font-semibold mb-7">
+          7-Loi applicable et juridiction compétente
+        </h2>
+
+        <h3 class="text-dark xs:w-full w-10/12 text-lg lg:text-xl font-semibold mb-7">
+          Loi applicable
+        </h3>
+        <p class="text-dark xs:w-full w-10/12 font-xs mb-7">
+          Ces mentions légales sont régies et interprétées conformément aux lois
+          de la France, sans tenir compte des conflits de lois. Vous acceptez
+          que les lois de la France s'appliquent à toute question ou litige
+          découlant de ou se rapportant à ces mentions légales et à
+          l'utilisation de ce site.
+        </p>
+
+        <h3 class="text-dark xs:w-full w-10/12 text-lg lg:text-xl font-semibold mb-7">
+          Juridiction compétente en cas de litige
+        </h3>
+        <p class="text-dark xs:w-full w-10/12 font-xs mb-7">
+          En cas de litige découlant de ou en rapport avec ces mentions légales
+          ou votre utilisation de ce site, vous acceptez que les tribunaux de la
+          France aient compétence exclusive pour résoudre le litige. Vous
+          acceptez de vous soumettre à la compétence personnelle de ces
+          tribunaux.
+        </p>
+      </div>
+    </Classic>
+  );
+};
+
+export default index;
